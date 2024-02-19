50b19c1a87d0ad226bf7be93ec3daf6e0e7bf995
Abdul Moaiz <moaizuk@gmail.com>
2024-02-19 22:58:33 +0000

    code reduction

--- Models/MokeEmployeeRepository.cs
+++ Models/MokeEmployeeRepository.cs
@@ -2,22 +2,22 @@
 {
     public class MokeEmployeeRepository : IEmployeeRepository
     {
-        private List<Employee> _employeeList;
+        private readonly List<Employee> _employeeList;
 
         public MokeEmployeeRepository()
         {
-            _employeeList = new List<Employee>()
-            {
-                new Employee() { Id = 1, Name = "Moaiz", Department = EnumDep.IT, Email = "moaizuk@gmail.com" },
-                new Employee() { Id = 2, Name = "Sarmad", Department = EnumDep.Payroll, Email = "sarmaduk@gmail.com" },
-                new Employee() { Id = 3, Name = "Zain", Department = EnumDep.HR, Email = "zainuk@gmail.com" },
-                new Employee() { Id = 4, Name = "Faisal", Department = EnumDep.IT, Email = "faisaluk@gmail.com" },
-                new Employee() { Id = 5, Name = "Abbas", Department = EnumDep.Payroll, Email = "abbasuk@gmail.com" },
-                new Employee() { Id = 6, Name = "Hassan", Department = EnumDep.None, Email = "hassanuk@gmail.com" },
-                new Employee() { Id = 7, Name = "Ahmad", Department = EnumDep.IT, Email = "ahmaduk@gmail.com" },
-                new Employee() { Id = 8, Name = "Aftab", Department = EnumDep.Payroll, Email = "aftabuk@gmail.com" },
-                new Employee() { Id = 9, Name = "Sheri", Department = EnumDep.HR, Email = "sheriuk@gmail.com" }
-            };
+            _employeeList =
+            [
+                new() { Id = 1, Name = "Moaiz", Department = EnumDep.IT, Email = "moaizuk@gmail.com" },
+                new() { Id = 2, Name = "Sarmad", Department = EnumDep.Payroll, Email = "sarmaduk@gmail.com" },
+                new() { Id = 3, Name = "Zain", Department = EnumDep.HR, Email = "zainuk@gmail.com" },
+                new() { Id = 4, Name = "Faisal", Department = EnumDep.IT, Email = "faisaluk@gmail.com" },
+                new() { Id = 5, Name = "Abbas", Department = EnumDep.Payroll, Email = "abbasuk@gmail.com" },
+                new() { Id = 6, Name = "Hassan", Department = EnumDep.None, Email = "hassanuk@gmail.com" },
+                new() { Id = 7, Name = "Ahmad", Department = EnumDep.IT, Email = "ahmaduk@gmail.com" },
+                new() { Id = 8, Name = "Aftab", Department = EnumDep.Payroll, Email = "aftabuk@gmail.com" },
+                new() { Id = 9, Name = "Sheri", Department = EnumDep.HR, Email = "sheriuk@gmail.com" }
+            ];
         }
 
         public IEnumerable<Employee> GetAllEmployees()
