7a8b5f75913a75a0a63cfd75638d35ad7a49a3e0
Abdul Moaiz <moaizuk@gmail.com>
2024-02-24 13:59:40 +0000

    Creating new employees using create function

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -5,5 +5,18 @@ namespace EmployeeManagement.Models
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<Employee> Employees { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = EnumDep.IT,
+                    Email = "mary@gmail.com"
+                }
+                );
+        }*/
     }
 }
